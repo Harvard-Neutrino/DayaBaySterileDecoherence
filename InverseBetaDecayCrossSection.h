c72946db74ed4bf556e30e7d8c03dbd23719c4dc
jsalvado <jor.salvado@gmail.com>
2021-05-03 09:16:38 +0200

    test

--- InverseBetaDecayCrossSection.h
+++ InverseBetaDecayCrossSection.h
@@ -4,7 +4,7 @@
 #include <cmath>
 
 namespace IBD {
-  // following hep-ph/9903554
+  // following hep-ph/9903554  
   static const double NeutrinoEnergyThreshold = 1.806; // MeV
   static const double DeltaNeutronToProtonMass = 1.29322; // MeV from PDG2018 mass differences
   static const double ElectronMass = 0.511; // MeV
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DayaBaySterileDecoherence
+# test
