0352bbb9ea404fbfb01ae4ff1ed9e8089e7aaa41
tbertolez <antoni.bertolez@qa.ub.edu>
2021-05-04 13:05:21 +0200

    Implemented function GetFlux (v2), now with comments.

--- Python implementation/HuberMullerFlux.py
+++ Python implementation/HuberMullerFlux.py
@@ -2,15 +2,32 @@ import math
 
 class reactor_isotope_flux:
     def __init__(self, isotope_name,flux_parameters):
+        """
+        Initialisation input:
+        isotope_name (str): a key to choose which isotope
+        we are studying. For instance, 'U235' or 'PU239'.
+        flux_parameters (dict): a dictionary, with the names of
+        the isotope as the keys, and a list of the flux_parameters
+        associated to each key.
+
+        Note to self: when the class is called, call it with
+        the right flux_parameters as an argument, either
+        hubber_muller or muller.
+        """
         self.isotope_name = isotope_name
         self.flux_parameters = flux_parameters
-        # When the class is called, call it with the
-        # right flux_parameters as an argument
-        # (either hubber_muller or muller).
 
     def GetFlux(self, Enu):
-        """ Returns the flux of the isotope, using the flux_parameters
-        from 1106.0687."""
+        """
+        Input:
+        The energy Enu of the outgoing neutrino (in MeV),
+        of which we want to know its flux.
+
+        Output:
+        The flux of the outgoing neutrino with energy Enu
+        after the beta decay of the isotope in isotope_name,
+        according to the formula and parameters in 1106.0687.
+        """
         exponent = 0.0
         for i in range(0,len(self.flux_parameters)):
             exponent += self.flux_parameters[self.isotope_name][i]*Enu**i
