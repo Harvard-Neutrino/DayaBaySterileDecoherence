Corrected normalisation. The problem is still there.

DayaBay/DayaBay.py
@@ -38,7 +38,7 @@ class DayaBay:
         # In principle, our analysis is flux-free, i.e. independent of the flux.
         # Therefore, the total normalisation of the flux is not important.
         # However, we consider an arbitrary large number of targets to prevent very small event expectations.
-        self.TotalNumberOfProtons = 1e53
+        self.TotalNumberOfProtons = {'EH1': 16.968688565113972e53, 'EH2': 15.783625798930741e53, 'EH3': 16.21670112543282e53}
 
         self.sets_names = DBP.exp_names
         self.reactor_names = DBP.reac_names
@@ -60,6 +60,7 @@ class DayaBay:
 
         self.AllData = DBD.all_data
         self.ObservedData = DBD.observed_data
+        self.PredictedDataNoOsc = DBD.predicted_data_noosc
         self.PredictedBackground = DBD.predicted_bkg
 
 
@@ -196,7 +197,7 @@ class DayaBay:
 
         # reactor loop ends
         # the two deltaEfine are to realise a trapezoidal numeric integration
-        return expectation*self.deltaEfine**2*self.EfficiencyOfHall[set_name]* self.TotalNumberOfProtons
+        return expectation*self.deltaEfine**2*self.EfficiencyOfHall[set_name]* self.TotalNumberOfProtons[set_name]
 
     def integrand(self,enu,L,model,erf,etf):
         """
@@ -267,7 +268,7 @@ class DayaBay:
 
         # reactor loop ends
         # only one trapezoidal numeric integration has been done
-        return expectation*self.deltaEfine*self.EfficiencyOfHall[set_name]*self.TotalNumberOfProtons
+        return expectation*self.deltaEfine*self.EfficiencyOfHall[set_name]*self.TotalNumberOfProtons[set_name]
 
 
     def get_expectation_unnorm_nobkg(self,model,do_we_integrate = False,imin = 0,imax = DBD.number_of_bins, do_we_average = False):
@@ -309,12 +310,14 @@ class DayaBay:
         number of events of "events" is the same as the one from observed data.
         Each experimental hall has a different factor.
         """
-        TotalNumberOfExpEvents = dict([(set_name,np.sum(self.ObservedData[set_name]))
+        TotalNumberOfExpEvents = dict([(set_name,np.sum(self.PredictedDataNoOsc[set_name]))
                                             for set_name in self.sets_names])
         TotalNumberOfBkg = dict([(set_name,np.sum(self.PredictedBackground[set_name]))
                                  for set_name in self.sets_names])
         norm = dict([(set_name,(TotalNumberOfExpEvents[set_name]-TotalNumberOfBkg[set_name])/np.sum(events[set_name]))
                      for set_name in self.sets_names])
+
+        print('norms: ', norm)
         return norm
 
 
@@ -354,8 +357,8 @@ class DayaBay:
 
         # We build the expected number of events for our model and we normalise so it has the same number of events of the data.
         exp_events = self.get_expectation_unnorm_nobkg(model,do_we_integrate = integrate, do_we_average = average)
-        norm = self.normalization_to_data(exp_events)
-        exp_events = dict([(set_name,exp_events[set_name]*norm[set_name]) for set_name in self.sets_names])
+        # norm = self.normalization_to_data(exp_events)
+        # exp_events = dict([(set_name,exp_events[set_name]*norm[set_name]) for set_name in self.sets_names])
 
         # We construct the nuissance parameters which minimise the Poisson probability
         # alpha_i = (dataEH1_i+dataEH2_i+dataEH3_i)/(expEH1_i+expEH2_i+expEH3_i)

NEOS/NEOS.py
@@ -30,7 +30,7 @@ class Neos:
         # In principle, our analysis is flux-free, i.e. independent of the flux.
         # Therefore, the total normalisation of the flux is not important.
         # However, we consider an arbitrary large number of targets to prevent very small event expectations.
-        self.TotalNumberOfProtons = 1e50
+        self.TotalNumberOfProtons = 4.76567e50
 
         self.sets_names = NEOSP.exp_names
         self.reactor_names = NEOSP.reac_names
@@ -53,6 +53,7 @@ class Neos:
 
         self.AllData = NEOSD.all_data
         self.PredictedData = NEOSD.predicted_data
+        self.PredictedDataHM = NEOSD.predicted_data_HM
         self.ObservedData = NEOSD.observed_data
         self.PredictedBackground = NEOSD.predicted_bkg
 
@@ -227,7 +228,7 @@ class Neos:
             # the two deltaEfine are to implement a trapezoidal numeric integration in etrue and erec
             # the dL is to implement a trapezoidal numeric integration in L
             # we divide by the total width 2W because we want an intensive quantity! It is an average, not a total sum.
-        return expectation*self.deltaEfine**2*dL/(2*W)*self.EfficiencyOfHall[set_name]* self.TotalNumberOfProtons
+        return expectation*self.deltaEfine**2*dL/(2*W)*self.EfficiencyOfHall[set_name]*self.TotalNumberOfProtons
 
 
     def integrand(self,enu,L,model,erf,etf):
@@ -356,6 +357,9 @@ class Neos:
 
     def normalization_to_data(self,events):
         """
+        Returns a normalization factor with which to normalise the expected events
+        to the predicated data according to SM.
+
         Input:
         events: a dictionary with a string key for each experimental hall, linking to
         a numpy array for some histogram of events. In principle, it should be
@@ -366,13 +370,15 @@ class Neos:
         number of events of "events" is the same as the one from NEOS data.
         """
 
-        TotalNumberOfEvents = dict([(set_name,np.sum(self.ObservedData[set_name]))
+        TotalNumberOfEvents = dict([(set_name,np.sum(self.PredictedDataHM[set_name]))
                                      for set_name in self.sets_names])
         TotalNumberOfBkg = dict([(set_name,np.sum(self.PredictedBackground[set_name]))
                                   for set_name in self.sets_names])
 
         norm = dict([(set_name,(TotalNumberOfEvents[set_name]-TotalNumberOfBkg[set_name])/np.sum(events[set_name]))
                      for set_name in self.sets_names])
+        print(events)
+        print('norm: ', norm)
         return norm
 
 
@@ -392,8 +398,8 @@ class Neos:
         # We build the expected number of events for our model and we roughly normalise so that is of the same order of the data.
         exp_events = self.get_expectation_unnorm_nobkg(model,do_we_integrate = integrate, do_we_average = False)
 
-        norm = self.normalization_to_data(exp_events)
-        exp_events = dict([(set_name,(exp_events[set_name]*norm[set_name]) +self.PredictedBackground[set_name]) for set_name in self.sets_names])
+        # norm = self.normalization_to_data(exp_events) # Huge mistake!
+        exp_events = dict([(set_name,exp_events[set_name] +self.PredictedBackground[set_name]) for set_name in self.sets_names])
 
         # For the NEOS single fit, there are no nuissance parameters. We just return the data.
         model_expectations = dict([(set_name,np.array([(exp_events[set_name][i],np.sqrt(exp_events[set_name][i]),

NEOS/NEOSData.py
@@ -39,7 +39,7 @@ def reconstruct_matrix_function(self,etrue,erec):
     """
     This function tries to mimic the response matrix in inset from figure 3(a)
     in 1610.05134. More information on the fit in 1609.03910.
-    
+
     Input:
     etrue, erec (float): the true and reconstructed energies.
 
@@ -109,6 +109,7 @@ def txt_to_array(filename, sep = ","):
 # 2: Number of background events, per day, per 100 keV
 # 3: Ratio of NEOS data to DayaBay data
 # 4: Error of ratio of NEOS data to DayaBay data
+# 5: Number of expected events according to HM flux, per day, per 100 keV
 
 norm = 180.
 # It is important to say: this normalisation factor must not include the deltaE
@@ -118,6 +119,7 @@ fudge_bkg = 1.
 all_data = {'NEOS': txt_to_array(dir+"AllData.dat")}
 observed_data  = {'NEOS': norm*fudge_data*txt_to_array(dir+"AllData.dat")[:-1,0]}
 predicted_data = {'NEOS': norm*fudge_data*txt_to_array(dir+"AllData.dat")[:-1,1]}
+predicted_data_HM = {'NEOS': norm*fudge_data*txt_to_array(dir+"AllData.dat")[:-1,5]}
 predicted_bkg = {'NEOS': norm*fudge_bkg*txt_to_array(dir+"AllData.dat")[:-1,2]}
 # predicted_bkg = {'NEOS':np.zeros([number_of_bins])}
 