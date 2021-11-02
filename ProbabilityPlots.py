8dabd886d6ab0f98222e9a8a7ff5ce68469bfd79
tbertolez <antoni.bertolez@fqa.ub.edu>
2021-11-02 19:07:16 +0100

    Update probability plot.

--- ProbabilityPlots.py
+++ ProbabilityPlots.py
@@ -18,10 +18,12 @@ plt.style.use(path_to_style+r"/paper.mplstyle")
 matplotlib.rcParams.update({'text.usetex': True})
 matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
 
+
+
 C = 80
 dm_DB = 0.25
 dm_NEOS = C/24
-print(dm_NEOS)
+# print(dm_NEOS)
 angl = 0.4
 SM_PW = Models.PlaneWaveSM()
 SM_WP = Models.WavePacketSM()
@@ -31,9 +33,9 @@ NEOS_PW = Models.PlaneWaveSterile(DM2_41 = dm_NEOS, Sin22Th14 = angl)
 NEOS_WP = Models.WavePacketSterile(DM2_41 = dm_NEOS, Sin22Th14 = angl)
 
 L_DB = C/0.25
-print(L_DB)
+# print(L_DB)
 L_NEOS = 24
-print(L_NEOS*dm_NEOS)
+# print(L_NEOS*dm_NEOS)
 
 axis = [1.3,12.,0.85,1]
 datx = np.arange(axis[0],axis[1],0.001)
@@ -46,15 +48,21 @@ prob_NEOS_WP =  [NEOS_WP.oscProbability(x,L_NEOS)/SM_WP.oscProbability(x,L_NEOS)
 margins = dict(left=0.14, right=0.96,bottom=0.15, top=0.9)
 plot,axx =plt.subplots(figsize = (7,5),gridspec_kw=margins)
 
+print(np.sqrt(L_NEOS*dm_NEOS/(4*np.sqrt(2)*2.1e-13))*1e-6)
+
 # axx.plot(datx,prob_DB_PW)
 # axx.plot(datx,prob_DB_WP)
 axx.grid(linestyle = '--')
 axx.tick_params(axis='x')
 axx.tick_params(axis='y')
+axx.set_xlim([1.8,12])
+axx.set_ylim([0.59,1.])
 axx.set_ylabel(r"$P_{3+1}/P_3$")
 axx.set_xlabel(r"$E (\text{MeV})$")
 axx.plot(datx,prob_NEOS_PW, label = 'Plane wave')
 axx.plot(datx,prob_NEOS_WP, label = 'Wave packet')
+axx.annotate(r'$E = E^{\text{coh}}$',(8.3,0.98), size = 14, color = 'k')
+axx.vlines(np.sqrt(L_NEOS*dm_NEOS/(4*np.sqrt(2)*2.1e-13))*1e-6, ymin = 0.6, ymax = 1., linestyle = '--', color = 'gray')
 axx.legend()
 plot.suptitle(r'$L\cdot \Delta m^2_{41} = 80 \text{ m}\cdot\text{eV}^2,\ \sin^2 2\theta_{14} = 0.4$')
 plot.savefig('Probability.pdf')
