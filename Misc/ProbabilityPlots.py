51174d2a2c6742f38dbae56712233fa506d38202
Toni Bertólez <antoni.bertolez@fqa.ub.edu>
2023-03-02 19:32:17 +0100

    Misc updated

--- Misc/ProbabilityPlots.py
+++ Misc/ProbabilityPlots.py
@@ -1,10 +1,10 @@
 import sys
 import os
-common_dir = '/Common_cython'
-main_dir = os.getcwd()[:-5]
-sys.path.append(main_dir+common_dir)
-sys.path.append(main_dir+"/DayaBay")
-sys.path.append(main_dir+"/NEOS")
+homedir = os.path.realpath(__file__)[:-len('Misc/ProbabilityPlots.py')]
+common_dir = 'Common_cython'
+sys.path.append(homedir+common_dir)
+sys.path.append(homedir+"/NEOS")
+sys.path.append(homedir+"/DayaBay")
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,8 +12,7 @@ import matplotlib
 
 import Models
 
-path_to_style = main_dir + common_dir
-dir = 'PlotData/'
+path_to_style = homedir + common_dir
 plt.style.use(path_to_style+r"/paper.mplstyle")
 matplotlib.rcParams.update({'text.usetex': True})
 matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
@@ -61,50 +60,50 @@ axx.tick_params(axis='y')
 axx.set_xlim([axis[0],axis[1]])
 axx.set_ylim([0.59,1.])
 axx.set_ylabel(r"$P_{3+1}/P_3$", fontsize = 24)
-axx.set_xlabel(r"$E (\text{MeV})$", fontsize = 24)
+axx.set_xlabel(r"$E (\textrm{MeV})$", fontsize = 24)
 axx.plot(datx,prob_NEOS_PW, label = 'Plane wave', color = color2)
 axx.plot(datx,prob_NEOS_WP, label = 'Wave packet', color = color3)
-axx.annotate(r'$E = E^{\text{coh}}$',(8.3,0.97), size = 20, color = 'k')
+axx.annotate(r'$E = E^{\textrm{coh}}$',(8.3,0.97), size = 20, color = 'k')
 axx.vlines(np.sqrt(L_NEOS*dm_NEOS/(4*np.sqrt(2)*2.1e-13))*1e-6, ymin = 0.6, ymax = 1., linestyle = '--', color = 'gray')
 axx.legend(bbox_to_anchor=(0,-0.31, 1, 0), loc="lower center", ncol = 2, fontsize = 20)
-plot.suptitle(r'$L\cdot \Delta m^2_{41} = 80 \text{ m}\cdot\text{eV}^2,\ \sin^2 2\theta_{14} = 0.4$')
-plot.savefig('Probability.pdf')
+plot.suptitle(r'$L\cdot \Delta m^2_{41} = 80 \textrm{ m}\cdot\textrm{eV}^2,\ \sin^2 2\theta_{14} = 0.4$')
+plot.savefig(homedir+'Misc/Probability.pdf')
 
 # -----------------------------------------------------------------
 
-dm = 1
+dm = 0.2
 sin2 = 0.1
 SM_PW = Models.PlaneWaveSM()
-SM_WP = Models.WavePacketSM()
 PW = Models.PlaneWaveSterile(DM2_41 = dm, Sin22Th14 = sin2)
 WP = Models.WavePacketSterile(DM2_41 = dm, Sin22Th14 = sin2)
 
-E = 2
-rat_ax = np.arange(0,7,0.001)
+E = 20
+axis = [1.3,16.,0.85,1]
+datx = np.arange(400,2500,1)
 # L = ratio*E
 sigmax = 2.1e-13
-Lcoh = 4*np.sqrt(2)*sigmax/(dm*1e-12)*E
-print(Lcoh)
+#Lcoh = 4*np.sqrt(2)*sigmax/(dm*1e-12)*E
+#print(Lcoh)
 
-prob_PW =  [PW.oscProbability(E,x*E) for x in rat_ax]
-prob_WP =  [WP.oscProbability(E,x*E) for x in rat_ax]
-
+prob_SM =  [SM_PW.oscProbability(E,x) for x in datx]
+prob_PW =  [PW.oscProbability(E,x) for x in datx]
+prob_WP =  [WP.oscProbability(E,x) for x in datx]
 
 plot,axx =plt.subplots(figsize = (7,6),gridspec_kw=margins)
-axx.plot(rat_ax,prob_PW, label = 'Plane wave', color = color2)
-axx.plot(rat_ax,prob_WP, label = 'Wave packet', color = color3)
+axx.plot(datx,prob_PW, label = 'Plane Wave', color = color2)
+axx.plot(datx,prob_WP, label = 'Wave packet', color = color3)
 
-axx.grid(linestyle = '--')
+#axx.grid(linestyle = '--')
 axx.tick_params(axis='x')
 axx.tick_params(axis='y')
-axx.set_ylabel(r"$P_{3+1}$", fontsize = 24)
-axx.set_xlabel(r"$L/E\ (\text{m}/\text{MeV})$", fontsize = 24)
-axx.set_xlim([0,7])
-axx.set_ylim([0.89,1.01])
+axx.set_ylabel(r"$\textrm{Probability}$", fontsize = 24)
+axx.set_xlabel(r"$L (\textrm{km})$", fontsize = 24)
+#axx.set_xlim([0,1500])
+#axx.set_ylim([0.0,1.0])
 
-axx.legend(bbox_to_anchor=(0,-0.31, 1, 0), loc="lower center", ncol = 2, fontsize = 20)
-axx.vlines(Lcoh, ymin = 0.9, ymax = 1., linestyle = '--', color = 'gray')
-axx.annotate(r'$L = L^{\text{coh}}$',(Lcoh*1.05,0.99), size = 20, color = 'k')
+#axx.legend(bbox_to_anchor=(0,-0.31, 1, 0), loc="lower center", ncol = 2, fontsize = 20)
+#axx.vlines(Lcoh, ymin = 0.9, ymax = 1., linestyle = '--', color = 'gray')
+#axx.annotate(r'$L = L^{\textrm{coh}}$',(Lcoh*1.05,0.99), size = 20, color = 'k')
 
-plot.suptitle(r'$E = %.1f\text{ MeV},\ \Delta m^2_{41} = 1\text{eV}^2,\ \sin^2 2\theta_{14} = 0.1$'%(E))
-plot.savefig('Probability_IsoDAR_E%.2f.pdf'%(E))
+#plot.suptitle(r'$E_\nu = %.1f\textrm{ MeV},\ \Delta m^2_{41} = 0.2\textrm{eV}^2,\ \sin^2 2\theta_{14} = 0.1$'%(E))
+plot.savefig(homedir+'Misc/Probability_L.png')
--- Misc/SuperFitPlots.py
+++ Misc/SuperFitPlots.py
@@ -1,19 +1,19 @@
 import sys
 import os
 import time
-common_dir = '/Common_cython'
-main_dir = os.getcwd()[:-5]
-print(main_dir)
-sys.path.append(main_dir+common_dir)
-sys.path.append(main_dir+"/GlobalFit")
-sys.path.append(main_dir+"/PROSPECT")
-sys.path.append(main_dir+"/DayaBay")
-sys.path.append(main_dir+"/NEOS")
-sys.path.append(main_dir+"/BEST")
+homedir = os.path.realpath(__file__)[:-len('Misc/SuperFitPlots.py')]
+common_dir = 'Common_cython/'
+sys.path.append(homedir+common_dir)
+sys.path.append(homedir+"NEOS/")
+sys.path.append(homedir+"DayaBay/")
+sys.path.append(homedir+"GlobalFit/")
+sys.path.append(homedir+"PROSPECT/")
+sys.path.append(homedir+"BEST/")
 
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
+from matplotlib.lines import Line2D
 
 import Models
 import GlobalFit as GF
@@ -23,11 +23,11 @@ import BEST
 import scipy.interpolate
 
 
-path_to_style = main_dir + common_dir
-dirGF = main_dir+'/GlobalFit/PlotData/'
-dirPS = main_dir+'/PROSPECT/PlotData/'
-dirBEST = main_dir+'/BEST/PlotData/'
-plotdir = 'AllFitFigures/'
+path_to_style = homedir + common_dir
+dirGF = homedir+'GlobalFit/PlotData/'
+dirPS = homedir+'PROSPECT/PlotData/'
+dirBEST = homedir+'BEST/PlotData/'
+plotdir = homedir+'Misc/AllFitFigures/'
 plt.style.use(path_to_style+r"/paper.mplstyle")
 matplotlib.rcParams.update({'text.usetex': True})
 matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
@@ -71,6 +71,8 @@ def txt_to_array(filename, sep = ",", section = 0):
 
 # print(dataBEST2[:18])
 
+sigma = 5.0e-4 # in nm
+
 fitterGF = GF.GlobalFit()
 fitterPS = PS.Prospect()
 fitterBEST = BEST.Best()
@@ -79,7 +81,7 @@ def getChi2(mass,angl,wave_packet = False):
     if wave_packet == False:
         model = Models.PlaneWaveSterile(Sin22Th14 = angl, DM2_41 = mass)
     else:
-        model = Models.WavePacketSterile(Sin22Th14 = angl, DM2_41 = mass)
+        model = Models.WavePacketSterile(Sin22Th14 = angl, DM2_41 = mass, Sigma = sigma)
     chi2GF = fitterGF.get_chi2(model)
     chi2PS = fitterPS.get_chi2(model)
     return chi2GF + chi2PS
@@ -88,7 +90,7 @@ def getBESTChi2(mass,angl,wave_packet = False):
     if wave_packet == False:
         model = Models.PlaneWaveSterile(Sin22Th14 = angl, DM2_41 = mass)
     else:
-        model = Models.WavePacketSterile(Sin22Th14 = angl, DM2_41 = mass)
+        model = Models.WavePacketSterile(Sin22Th14 = angl, DM2_41 = mass, Sigma = sigma)
     chi2 = fitterBEST.get_chi2(model)
     return chi2
 
@@ -102,10 +104,11 @@ def stylize(axxis,contours,t_ax = [1e-3,1], m_ax = [1e-2,10]):
     axxis.set_xlabel(r"$\sin^2 2 \theta_{14}$", fontsize = 24)
     axxis.set_xlim([4e-3,1])
     axxis.set_ylim([8e-2,10])
-    labels = [r'$1\sigma$ (68\% C.L.)',r'$2\sigma$ (95\% C.L.)',r'$3\sigma$ (99\% C.L.)']
-    for i in range(3):
-        contours.collections[i].set_label(labels[i])
-    axxis.legend(loc = 'lower right', fontsize = 18)
+    legend_elements = [Line2D([0], [0], color=color1, ls = '-', lw=2, label=r'$1\sigma$ (68\% C.L.)'),
+                       Line2D([0], [0], color=color2, ls = '-', lw=2, label=r'$2\sigma$ (95\% C.L.)'),
+                       Line2D([0], [0], color=color3, ls = '-', lw=2, label=r'$3\sigma$ (99\% C.L.)'),
+                       Line2D([0], [0], marker='+', color='c', lw = 0, label='Best Fit', markerfacecolor='b', markersize=8)]
+    axxis.legend(handles = legend_elements, loc = 'upper left', fontsize = 16)
 
 
 titlesize = 13.
@@ -161,7 +164,7 @@ data_PW = np.vstack((yy.flatten(),xx.flatten(),(chi2GF_PW+chi2PS_PW).flatten()))
 print(data_PW)
 # print(x.shape)
 
-file = open('AllFitPointsPW.dat','w')
+file = open(homedir+'Misc/AllFitPointsPW.dat','w')
 for i in range(x.shape[0]):
     for j in range(y.shape[0]):
         file.write('{0:1.8f},{1:1.6f},{2:7.4f}\n'.format(data_PW[i+200*j,0],data_PW[i+200*j,1],data_PW[i+200*j,2]))
@@ -181,51 +184,25 @@ min_index_BEST = np.where((dataBEST_PW[:,2] == np.min(dataBEST_PW[:,2])))[0][0]
 bestfitBEST_PW = dataBEST_PW[min_index_BEST]
 
 
-# PLOT WITH RESPECT TO THE BEST FIT
-# ----------------------------------
-figBF,axBF = plt.subplots(figsize = size,gridspec_kw=margins)
-
-conts = axBF.tricontour(data_PW[:,1],data_PW[:,0],(data_PW[:,2]-bestfit[2]),levels = [2.30,6.18,11.83], colors = [color1,color2,color3])
-axBF.scatter(bestfit[1],bestfit[0],marker = '+', label = r'Best fit')
-# axBF.scatter(data_PW[:,1],data_PW[:,0],marker = '+', s = 1.) # This tells us the resolution of our table
-
-stylize(axBF,conts)
-
-figBF.suptitle(r'Best fit:  $\Delta m^2_{41} = %.2f \textrm{ eV}^2$, $\sin^2 2\theta_{14} = %.3f$. Total $\chi^2 = %.2f$'%(bestfit[0],bestfit[1], bestfit[2]), fontsize = titlesize)
-figBF.savefig(plotdir+'PWContour_bestfit.png')
-
-
-# PLOT WITH RESPECT TO THE NULL HYPOTHESIS
-# -----------------------------------------
-
-figNH,axNH = plt.subplots(figsize = size, gridspec_kw = margins)
-
-conts = axNH.tricontour(data_PW[:,1],data_PW[:,0],(data_PW[:,2]-null_hyp_PW),levels = [2.30,6.18,11.83], colors = [color1,color2,color3])
-axNH.scatter(bestfit[1],bestfit[0],marker = '+', label = 'Our best fit')
-# axNH.scatter(data_PW[:,1],data_PW[:,0],marker = '+', s = 1.) # This tells us the resolution of our table
-
-stylize(axNH,conts)
-
-figNH.suptitle('Null hypothesis: total $\chi^2 = %.2f$'%(null_hyp_PW), fontsize = titlesize)
-figNH.savefig(plotdir+'PWContour_nullhyp.png')
-
-
-
 # -------------------------------------------------
 # STERILE WAVE PACKET CONTOUR - WAVE PACKET FORMALISM
 # -------------------------------------------------
 
-dataGF_WP = txt_to_array(dirGF+'WPSterileChi2.dat')
+if sigma == 2.1e-4:
+    suffix = ''
+elif sigma == 2.1e-3:
+    suffix = '_2.1e-3'
+elif sigma == 5.0e-4:
+    suffix = '_5.0e-4'
+
+dataGF_WP = txt_to_array(dirGF+'WPSterileChi2'+suffix+'.dat')
 dataGF_WP = np.unique(dataGF_WP,axis=0) # We remove duplicates from the list
-dataPS_WP = txt_to_array(dirPS+'WPSterileChi2.dat')
+dataPS_WP = txt_to_array(dirPS+'WPSterileChi2'+suffix+'.dat')
 dataPS_WP = np.unique(dataPS_WP,axis=0) # We remove duplicates from the list
-dataBEST_WP = txt_to_array(dirBEST+'WPSterileChi2.dat')
+dataBEST_WP = txt_to_array(dirBEST+'WPSterileChi2'+suffix+'.dat')
 dataBEST_WP = np.unique(dataBEST_WP,axis=0) # We remove duplicates from the list
 begin = time.time()
-# print(dataGF_WP[:,0],dataGF_WP[:,1],dataGF_WP[:,2])
-# chi2GF = scipy.interpolate.interp2d(dataGF_WP[:,0],dataGF_WP[:,1],dataGF_WP[:,2])
-# chi2PS = scipy.interpolate.interp2d(dataPS_WP[:,0],dataPS_WP[:,1],dataPS_WP[:,2])
-# print(time.time()-begin)
+
 
 y = np.logspace(np.log10(0.08),1,200)
 x = np.logspace(-3,0,200)
@@ -242,7 +219,8 @@ chi2PS_WP = scipy.interpolate.griddata(pointsPS_WP, valuesPS_WP, (yy, xx), metho
 
 data_WP = np.vstack((yy.flatten(),xx.flatten(),(chi2GF_WP+chi2PS_WP).flatten())).transpose()#
 
-file = open('AllFitPointsWP.dat','w')
+
+file = open(homedir+'Misc/AllFitPointsWP'+suffix+'.dat','w')
 for i in range(x.shape[0]):
     for j in range(y.shape[0]):
         file.write('{0:1.8f},{1:1.6f},{2:7.4f}\n'.format(data_WP[i+200*j,0],data_WP[i+200*j,1],data_WP[i+200*j,2]))
@@ -262,36 +240,44 @@ min_index_BEST = np.where((dataBEST_WP[:,2] == np.min(dataBEST_WP[:,2])))[0][0]
 bestfitBEST_WP = dataBEST_WP[min_index_BEST]
 
 
-# PLOT WITH RESPECT TO THE BEST FIT
-# ----------------------------------
-figBF,axBF = plt.subplots(figsize = size, gridspec_kw = margins)
+# DATA FROM MINIMUM WAVE PACKET WIDTH
+# ------------------------------------------------------------------------
 
-conts = axBF.tricontour(data_WP[:,1],data_WP[:,0],(data_WP[:,2]-bestfit[2]),levels = [2.30,6.18,11.83], colors = [color1,color2,color3])
-axBF.scatter(bestfit[1],bestfit[0],marker = '+', label = r'Best fit')
-# axBF.scatter(data_WP[:,1],data_WP[:,0],marker = '+', s = 1.) # This tells us the resolution of our table
-
-stylize(axBF,conts)
-
-figBF.suptitle(r'Best fit:  $\Delta m^2_{41} = %.2f \textrm{ eV}^2$, $\sin^2 2\theta_{14} = %.3f$. Total $\chi^2 = %.2f$'%(bestfit[0],bestfit[1], bestfit[2]), fontsize = titlesize)
-figBF.savefig(plotdir+'WPContour_bestfit.png')
+dataGF_WP0 = txt_to_array(dirGF+'WPSterileChi2.dat')
+dataGF_WP0 = np.unique(dataGF_WP0,axis=0) # We remove duplicates from the list
+dataPS_WP0 = txt_to_array(dirPS+'WPSterileChi2.dat')
+dataPS_WP0 = np.unique(dataPS_WP0,axis=0) # We remove duplicates from the list
+dataBEST_WP0 = txt_to_array(dirBEST+'WPSterileChi2.dat')
+dataBEST_WP0 = np.unique(dataBEST_WP0,axis=0) # We remove duplicates from the list
 
 
-# PLOT WITH RESPECT TO THE NULL HYPOTHESIS
-# -----------------------------------------
+y = np.logspace(np.log10(0.08),1,200)
+x = np.logspace(-3,0,200)
+yy,xx = np.meshgrid(y,x)
 
-figNH,axNH = plt.subplots(figsize = size, gridspec_kw = margins)
+pointsGF_WP0 = dataGF_WP0[:,:2]
+valuesGF_WP0 = dataGF_WP0[:,2]
+chi2GF_WP0 = scipy.interpolate.griddata(pointsGF_WP0, valuesGF_WP0, (yy, xx), method='cubic',fill_value= 0)
 
-conts = axNH.tricontour(data_WP[:,1],data_WP[:,0],(data_WP[:,2]-null_hyp_WP),levels = [2.30,6.18,11.83], colors = [color1,color2,color3])
-axNH.scatter(bestfit[1],bestfit[0],marker = '+', label = 'Our best fit')
-# axNH.scatter(data_WP[:,1],data_WP[:,0],marker = '+', s = 1.) # This tells us the resolution of our table
-
-stylize(axNH,conts)
-
-figNH.suptitle('Null hypothesis: total $\chi^2 = %.2f$'%(null_hyp_WP), fontsize = titlesize)
-figNH.savefig(plotdir+'WPContour_nullhyp.png')
+pointsPS_WP0 = dataPS_WP0[:,:2]
+valuesPS_WP0 = dataPS_WP0[:,2]
+chi2PS_WP0 = scipy.interpolate.griddata(pointsPS_WP0, valuesPS_WP0, (yy, xx), method='cubic',fill_value = 0)
 
 
+data_WP0 = np.vstack((yy.flatten(),xx.flatten(),(chi2GF_WP0+chi2PS_WP0).flatten())).transpose()#
 
+# We find which is the point with minimum chi2, i.e. our best fit.
+min_index0 = np.where((data_WP0[:,2] == np.min(data_WP0[:,2])))[0][0]
+print(min_index0)
+bestfit0 = data_WP0[min_index0]
+print('Best fit values and chi2: ',bestfit0)
+
+# We find which is the chi2 of the null hypothesis
+null_hyp_WP0 = getChi2(0,0, wave_packet = True)
+print('Null hyp chi2: ',null_hyp_WP0)
+
+min_index_BEST0 = np.where((dataBEST_WP0[:,2] == np.min(dataBEST_WP0[:,2])))[0][0]
+bestfitBEST_WP0 = dataBEST_WP0[min_index_BEST0]
 
 # ----------------------------------------------
 # 2SIGMA PLOT COMPARISON
@@ -305,9 +291,7 @@ fig_comp,ax_comp = plt.subplots(figsize = size, gridspec_kw = margins)
 # ax_comp.fill_between(dataBEST2[:,0],dataBEST2[:,1], np.interp(dataBEST2[:,0],dataBEST2[:18,0],dataBEST2[:18,1]), alpha = 0.4, color = color1, zorder = 1)
 
 cont_PW = ax_comp.tricontour(data_PW[:,1],data_PW[:,0],(data_PW[:,2]-null_hyp_PW),levels = [6.18], colors = color2, zorder = 10)
-cont_PW.collections[0].set_label(r'$2\sigma$ Plane wave')
 cont_WP = ax_comp.tricontour(data_WP[:,1],data_WP[:,0],(data_WP[:,2]-null_hyp_WP),levels = [6.18], colors = color3, zorder = 20)
-cont_WP.collections[0].set_label(r'$2\sigma$ Wave packet')
 # ax_comp.scatter(dataBEST_PW[:,1],dataBEST_PW[:,0],marker = '+', s = 1.) # This tells us the resolution of our table
 
 
@@ -341,5 +325,62 @@ ax_comp.annotate("", xy=(solar90*1.5,1.2e-1), xytext=(solar90,1.2e-1),arrowprops
 ax_comp.legend(loc = 'upper left', fontsize = 18)
 ax_comp.legend(proxy,[r'$2\sigma$ Plane wave',r'$2\sigma$ Wave packet',r'BEST $2\sigma$ PW',r'BEST $2\sigma$ WP', 'Solar'],loc = 'upper left', fontsize = 17)
 
-fig_comp.savefig(plotdir+'ContourComparison.pdf')
-fig_comp.savefig(plotdir+'ContourComparison.png')
+
+fig_comp.savefig(plotdir+'ContourComparison'+suffix+'.pdf')
+fig_comp.savefig(plotdir+'ContourComparison'+suffix+'.png')
+
+
+# DIFFERENT WP WIDTH COMPARISON
+# ---------------------------------------
+
+
+margins = dict(left=0.16, right=0.97,bottom=0.11, top=0.97)
+fig_comp,ax_comp = plt.subplots(figsize = size, gridspec_kw = margins)
+# contBEST = ax_comp.plot(dataBEST1[:,0],dataBEST1[:,1], label = r'BEST $2\sigma$', color = color1, zorder = 2)
+# ax_comp.fill_between(dataBEST1[:,0],dataBEST1[:,1],10, alpha = 0.4, color = color1, zorder = 0, label = 'BEST+SAGE\n+GALLEX'+r' $2\sigma$')
+# contBEST = ax_comp.plot(dataBEST2[:,0],dataBEST2[:,1], color = color1, zorder = 3)
+# ax_comp.fill_between(dataBEST2[:,0],dataBEST2[:,1], np.interp(dataBEST2[:,0],dataBEST2[:18,0],dataBEST2[:18,1]), alpha = 0.4, color = color1, zorder = 1)
+
+cont_PW = ax_comp.tricontour(data_PW[:,1],data_PW[:,0],(data_PW[:,2]-null_hyp_PW),levels = [6.18], colors = color2, zorder = 10)
+cont_WP = ax_comp.tricontour(data_WP[:,1],data_WP[:,0],(data_WP[:,2]-null_hyp_WP),levels = [6.18], colors = color1, zorder = 20)
+cont_WP0 = ax_comp.tricontour(data_WP0[:,1],data_WP0[:,0],(data_WP0[:,2]-null_hyp_WP0),levels = [6.18], colors = color3, zorder = 20)
+# ax_comp.scatter(dataBEST_PW[:,1],dataBEST_PW[:,0],marker = '+', s = 1.) # This tells us the resolution of our table
+
+
+cont_BEST_PW = ax_comp.tricontourf(dataBEST_PW[:,1],dataBEST_PW[:,0],(dataBEST_PW[:,2]-bestfitBEST_PW[2]), levels = [0.0,6.18], colors = color2, alpha = 0.3, zorder = 6)
+cont_BEST_WP = ax_comp.tricontourf(dataBEST_WP[:,1],dataBEST_WP[:,0],(dataBEST_WP[:,2]-bestfitBEST_WP[2]), levels = [0.0,6.18], colors = color1, alpha = 0.3, zorder = 5)
+cont_BEST_PW = ax_comp.tricontour(dataBEST_PW[:,1],dataBEST_PW[:,0],(dataBEST_PW[:,2]-bestfitBEST_PW[2]), levels = [6.18], colors = color2, linewidths = 1,zorder = 6.5)
+cont_BEST_WP = ax_comp.tricontour(dataBEST_WP[:,1],dataBEST_WP[:,0],(dataBEST_WP[:,2]-bestfitBEST_WP[2]), levels = [6.18], colors = color1, linewidths = 1,zorder = 5.5)
+cont_BEST_WP0 = ax_comp.tricontourf(dataBEST_WP0[:,1],dataBEST_WP0[:,0],(dataBEST_WP0[:,2]-bestfitBEST_WP0[2]), levels = [0.0,6.18], colors = color3, alpha = 0.3, zorder = 5)
+cont_BEST_WP0 = ax_comp.tricontour(dataBEST_WP0[:,1],dataBEST_WP0[:,0],(dataBEST_WP0[:,2]-bestfitBEST_WP0[2]), levels = [6.18], colors = color3, linewidths = 1,zorder = 5.5)
+
+proxy = [plt.Line2D([0], [0], color=color2, lw=2), plt.Line2D([0], [0], color=color1, lw=2), plt.Line2D([0],[0],color = color3, lw = 2),
+        #  plt.Rectangle((0.1,0.1),0.8,0.8,fc = color2, alpha = 0.3),plt.Rectangle((0.1,0.1),0.8,0.8,fc = color1, alpha = 0.3), plt.Rectangle((0.1,0.1),0.8,0.8,fc = color3, alpha = 0.3),
+         plt.Line2D([0],[0], color = 'gray', ls = 'dashed', lw = 2)]
+
+# ax_comp.annotate('DB+NEOS', xy = (1.25e-3,5), size = 42)
+ax_comp.grid(linestyle = '--')
+ax_comp.tick_params(axis='x')
+ax_comp.tick_params(axis='y')
+ax_comp.set_xscale('log')
+ax_comp.set_yscale('log')
+ax_comp.set_ylabel(r"$\Delta m^2_{41} (\textrm{eV}^2)$", fontsize = 24)
+ax_comp.set_xlabel(r"$\sin^2 2 \theta_{14}$", fontsize = 24)
+ax_comp.set_xlim([4e-3,1])
+ax_comp.set_ylim([8e-2,10])
+
+ax_comp.vlines(solar90,0.08,10,colors='gray',linestyles = 'dashed')
+# ax_comp.vlines(solar99,0.08,10,colors='gray',linestyles = 'dotted')
+ax_comp.annotate("", xy=(solar90*1.5,1.2e-1), xytext=(solar90,1.2e-1),arrowprops=dict(arrowstyle="->", color = 'gray', lw = 1), zorder = 101)
+
+ax_comp.legend(proxy,[r'Plane wave',
+                      r'$\sigma_x = 5.0\times 10^{-4}\ \textrm{nm}$',
+                      r'$\sigma_x = 2.1\times 10^{-4}\ \textrm{nm}$', 
+                    #   'Gallium PW', 
+                    #   r'Gal $\sigma_x = 5.0\times 10^{-4}\ \textrm{nm}$',
+                    #   r'Gal $ \sigma_x = 2.1\times 10^{-4}\ \textrm{nm}$',
+                      'Solar'],loc = 'upper left', fontsize = 15)
+
+
+fig_comp.savefig(plotdir+'ContourComparison_3.pdf')
+fig_comp.savefig(plotdir+'ContourComparison_3.png')
